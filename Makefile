b91af18ba0c4aeef4ef56196ed8e0a4da939ac00
George Bolis <bolis@usc.edu>
2022-10-20 20:32:31 +0000

    Almost all done

--- Makefile
+++ Makefile
@@ -1,5 +1,6 @@
 CXX := g++
 CXXFLAGS := -g -Wall --std=c++11
+GTESTLIBS := -lgtest -lgtest_main -lpthread
 VALGRIND := valgrind --tool=memcheck --leak-check=yes
 
 all: llrec-test
@@ -7,6 +8,8 @@ all: llrec-test
 #-----------------------------------------------------
 # ADD target(s) to build your llrec-test executable
 #-----------------------------------------------------
+llrec.o	: llrec.cpp llrec.h
+	$(CXX) $(CXXFLAGS) -c $^ -o $@
 llrec-test : llrec-test.cpp llrec.cpp llrec.h 
 	$(CXX) $(CXXFLAGS) $^ -o $@
 
--- heap.h
+++ heap.h
@@ -2,6 +2,17 @@
 #define HEAP_H
 #include <functional>
 #include <stdexcept>
+#include <vector>
+#include <iostream>
+
+/*
+  1. Stack_test works great! DONE
+  2. llpivot works great but llfilter doesn't print DONE
+  3. ! heap implmentation (missing last test) "cd heap_test" "make test"
+  4. logicsim gives one wrong answer "cd logicsim" "make" "./logicsim single_and.txt" make sure pics are good and how to upload them. DONE
+  5. upload tree diagrams from part 4 DONE
+*/
+
 
 template <typename T, typename PComparator = std::less<T> >
 class Heap
@@ -15,7 +26,7 @@ public:
    *          as an argument and returns a bool if the first argument has
    *          priority over the second.
    */
-  Heap(int m=2, PComparator c = PComparator());
+  Heap(int m = 2, PComparator c = PComparator());
   /**
   * @brief Destroy the Heap object
   * 
@@ -53,13 +64,24 @@ public:
   size_t size() const;
 private:
   /// Add whatever helper functions and data members you need below
-  std::vector<object> curheap;
+  std::vector<T> curheap; 
   int m;
-  PComparator c; //don't worry about min/max heap
+  PComparator c = PComparator(); //don't worry about min/max heap
   // c(left child, right child)
-  void heapify(int i);
-  //heapfiy function
+  void heapify(int i); 
+  //heapfiy or trickle down function
+  void trickleUp(int k);
+  //void printHeap(std::vector<T>& curheap);
 };
+
+template <typename T, typename PComparator>
+Heap<T, PComparator>::Heap(int m_, PComparator c_){
+  m = m_;
+  //c = c_;
+}
+
+template <typename T, typename PComparator>
+Heap<T, PComparator>::~Heap(){}
 
 // Add implementation of member functions here
 
@@ -69,22 +91,22 @@ template <typename T, typename PComparator>
 T const & Heap<T,PComparator>::top() const{
   // Here we use exceptions to handle the case of trying
   // to access the top element of an empty heap
-  try{
     if(empty()){
       // ================================
       // throw the appropriate exception
       // ================================
       if (curheap.empty()){
-        throw out_of_range("Index is out of range(Heap underflow)");
+        throw std::out_of_range("Index is out of range(Heap underflow)");
       }
     }
     // If we get here we know the heap has at least 1 item
     // Add code to return the top element
     return curheap[0];
+  /*
+  catch(const std::out_of_range){
+    std::cout << "Invalid: not enough items in list" << std::endl;
   }
-  catch(const out_of_range){
-    cout << "Invalid: not enough items in list" << endl;
-  }
+  */
 }
 
 
@@ -92,33 +114,38 @@ T const & Heap<T,PComparator>::top() const{
 // calling top on an empty heap
 template <typename T, typename PComparator>
 void Heap<T,PComparator>::pop(){
-  try{
-    if(empty()){
       // ================================
       // throw the appropriate exception
       // ================================
-      if (curheap.empty()){
-        throw out_of_range("Index is out of range(Heap underflow)");
-      }
+    if (curheap.empty()){
+      throw std::out_of_range("Index is out of range(Heap underflow)");
     }
+    // printHeap(curheap);
     // switch the top with the back one
-    curheap[0] = curheap[curheap.size() - 1]; // back item?
+    std::swap(curheap.front(), curheap.back()); // back item?
     curheap.pop_back();
+
+    // NOTE CHANGE
+    if (curheap.size() == 1) {
+      return ;
+    }
     // need heapify function for min heap and max heap
     // call heapify here on 0
-    heapify(0);
-  }
-
-  catch(const out_of_range){
-    cout << "Invalid: not enough items in list" << endl;
-  }
+    this->heapify(0);
 }
+  /*
+  catch(const std::out_of_range){
+    std::cout << "Invalid: not enough items in list" << std::endl;
+  }
+  */
 
 template <typename T, typename PComparator>
 void Heap<T,PComparator>::push(const T& item){
   // inserting new element then calling heapify 
+  // printHeap(curheap);
   curheap.push_back(item);
-  heapify(curheap.size()-1);
+  this->trickleUp(curheap.size() - 1);
+  // size++;
 }
 
 template <typename T, typename PComparator>
@@ -134,33 +161,59 @@ bool Heap<T,PComparator>::empty() const{
 template <typename T, typename PComparator>
 size_t Heap<T,PComparator>::size() const{
   // check this
-  int size = 0;
-  for (int i = 0; i < curheap.size(); i++){
-    size++;
-  }
-  return size;
+  // int size = curheap.size();
+  return curheap.size();
 }
 
 template <typename T, typename PComparator>
 void Heap<T,PComparator>::heapify(int i){
+  
   // check this
   int children[m]; //m-ary so m-children for each node
-  if (/* leaf node*/){
-    // it is a leaf node 
+  size_t size = (m*i) + 1;
+  if (size >= curheap.size()){
+    // it does not exists 
     return;
   }
-  int smallerChild = (m*i) -1 //start with left child
-  int nextChild = smallerChild + 1; // go to next child
-  if(nextChild != NULL) { // next child exists
-    if(c(nextChild, smallerChild)){
-      smallerChild = nextChild;
+  size_t smallerChild = (m*i) +1; //start with left child
+  for (size_t j = 1; j < m; j++){
+    if ((smallerChild + j) >= curheap.size()){
+      // nothing to compare to...
+      // do i break here?
+      break;
     }
+    /*
+    if (c(curheap[smallerChild], curheap[smallerChild + j])){
+      smallerChild = smallerChild + j;
+    }
+    */
+    if (c(curheap[smallerChild + j], curheap[smallerChild])){
+      smallerChild = smallerChild + j;
+    } 
   }
-  if(c(i, smallerChild)){ 
-    swap(curheap[i], curheap[smallerChild]); 
+  if(c(curheap[smallerChild], curheap[i])){ 
+    std::swap(curheap[i], curheap[smallerChild]); 
     heapify(smallerChild);
   }
 }
+
+// should be done
+template <typename T, typename PComparator>
+void Heap<T,PComparator>::trickleUp(int k){
+  int parent = (k-1)/m; // 0-based indexing?
+  while ((parent >= 0) && c(curheap[k], curheap[parent])){
+    std::swap(curheap[k], curheap[parent]);
+    k = parent;
+    parent = (k-1)/m; 
+  }
+}
+
+// template <typename T, typename PComparator>
+// void Heap<T, PComparator>::printHeap(std::vector<T> &curheap){
+//   for (int d = 0; d < curheap.size(); d++){
+//     std::cout << curheap[d] << std::endl;
+//   }
+// }
 
 #endif
 

5401b60bb6009d67b9f40b526ba182c8c8c23c29
George Bolis <bolis@usc.edu>
2022-11-29 14:10:18 -0800

    Update llrec.cpp

--- llrec.cpp
+++ llrec.cpp
@@ -5,59 +5,19 @@
 //*********************************************
 
 void llpivot(Node *&head, Node *&smaller, Node *&larger, int pivot) {
-  smaller = NULL;
-  larger = NULL;
-  Node* smallPoint = NULL;
-  Node* largePoint =  NULL;
-  split(head, smaller, larger, pivot, smallPoint, largePoint);
-  return;
-}
-
-void split(Node *&head, Node *&smaller, Node *&larger, int pivot, Node *&smallPoint, Node *&largePoint){
   if (head == nullptr){
-    return;
+    larger = nullptr;
+    smaller = nullptr;
   }
-  if (head->val > pivot){
-    // means we are dealing with "larger"
-    if (larger == nullptr || larger == NULL){
-      larger = head;
-      largePoint = head;
-      // updating the next largepoint and head
-      largePoint->next = NULL;
-      head = head->next;
-      // recursive call to split
-      split(head, smaller, larger, pivot, smallPoint, largePoint);
-    }
-    else{
-      largePoint = head;
-      largePoint->next = head;
-      // updating the next largepoint and head
-      largePoint->next = NULL;
-      head = head->next;
-      // recursive call to split
-      split(head, smaller, larger, pivot, smallPoint, largePoint);
-    }
+  else if (head->val > pivot){
+    larger = head;
+    head = head->next;
+    llpivot(head, smaller, larger->next, pivot);
   }
-  else if (head->val <= pivot){
-    // means we are dealing with "smaller"
-    if (smaller == nullptr || smaller == NULL){
-      smaller = head;
-      smallPoint = head;
-      // updating the next smallpoint and head
-      head = head->next;
-      smallpoint->next = NULL;
-      // recursive call to split
-      split(head, smaller, larger, pivot, smallPoint, largePoint);
-    }
-    else{
-      smallPoint = head; 
-      smallPoint->next = head;
-      // updating the next smallpoint and head
-      head = head->next;
-      smallpoint->next = NULL;
-      // recursive call to split
-      split(head, smaller, larger, pivot, smallPoint, largePoint);
-    }
+  else{
+    smaller = head;
+    head = head->next;
+    llpivot(head, smaller->next, larger, pivot);
   }
 }
 
